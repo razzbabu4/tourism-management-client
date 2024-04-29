38941e2f2835e463f74e7011c03ee84c7544215e
Tareq <razzbabut@gmail.com>
2024-04-30 03:09:41 +0600

    added footer to all pages except error page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
     return (
-        <footer className="px-4 divide-y dark:bg-gray-100 dark:text-gray-800 mx-auto">
+        <footer className="px-4 divide-y dark:bg-gray-100 dark:text-gray-800 mx-auto mt-6">
             <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
                 <div className="lg:w-1/3">
                     <a rel="noopener noreferrer" href="#" className="flex justify-center space-x-3 lg:justify-start">
